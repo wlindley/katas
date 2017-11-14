426f4ec15c4b0cd190c74747ae45ff6aec75fa1d
Walker Lindley <walker@walkerlindley.com>
2017-11-13 21:15:56 -0800

    Implement through step 3 of the string calculator kata

--- rust/string_sum/src/lib.rs
+++ rust/string_sum/src/lib.rs
@@ -1,7 +1,41 @@
+fn delimeter(c:char) -> bool {
+    ',' == c || '\n' == c
+}
+
+fn safe_parse(number:&str) -> u32 {
+    match number.parse::<u32>() {
+        Ok(v) => v,
+        Err(_) => 0
+    }
+}
+
+pub fn add(numbers:&str) -> u32 {
+    numbers.split(delimeter)
+           .map(safe_parse)
+           .sum()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn returns_0_for_empty_string() {
+        assert_eq!(0, add(""));
+    }
+
+    #[test]
+    fn returns_1_for_string_with_1() {
+        assert_eq!(1, add("1"));
+    }
+
+    #[test]
+    fn returns_3_for_string_with_1_and_2() {
+        assert_eq!(3, add("1,2"));
+    }
+
+    #[test]
+    fn supports_newline_delimeter() {
+        assert_eq!(6, add("2\n3,1"));
     }
 }
