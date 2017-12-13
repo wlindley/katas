d334b1598ba3fa58b8035378ff0dc05203f12e83
Walker Lindley <walker@plutovr.com>
2017-12-12 17:05:27 -0800

    Start setting up loop

--- javascript/drop7/src/index.js
+++ javascript/drop7/src/index.js
@@ -1,8 +1,16 @@
 const Drop7 = require('./drop7');
 const ConsoleView = require('./console-view');
+const readline = require('readline');
 
 const cols = 4;
 const rows = 4;
 
 const app = new Drop7(rows, cols, new ConsoleView());
 app.start();
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+rl.on('line', line => {
+	
+});
