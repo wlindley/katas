ffda42817f8e9cb0d5f4190dfd82a1399eff7f49
Walker Lindley <walker@walkerlindley.com>
2018-01-09 21:51:27 -0800

    Implement real file IO

--- rust/rot13/src/lib.rs
+++ rust/rot13/src/lib.rs
@@ -1,5 +1,8 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn rotate_file(input: String, output: String) {
-    rotate_impl(input, output, &StandardFileIO {});
+    rotate_impl(input, output, &StandardFileIO::new());
 }
 
 fn rotate_impl<T>(input: String, output: String, file_io: &T) where T: FileIO {
@@ -31,18 +34,28 @@ trait FileIO {
 
 struct StandardFileIO;
 
+impl StandardFileIO {
+    fn new() -> StandardFileIO {
+        StandardFileIO {}
+    }
+}
+
 impl FileIO for StandardFileIO {
      fn read(&self, filename: String) -> String {
-         String::default()
+         let mut file = File::open(filename).expect("file not found");
+         let mut contents = String::default();
+         file.read_to_string(&mut contents).expect("could not read file");
+         contents
      }
 
      fn write(&self, filename: String, contents: String) {
-
+         let mut file = File::create(filename).expect("could not create file");
+         file.write_all(contents.as_bytes()).expect("failed to write file contents");
      }
 }
 
 #[cfg(test)]
-mod tests {
+mod rotate_tests {
     use super::*;
     use std::cell::RefCell;
 
@@ -63,7 +76,7 @@ mod tests {
     }
 
     impl FileIO for MockFileIO {
-        fn read(&self, filename: String) -> String {
+        fn read(&self, _filename: String) -> String {
             self.read_contents.clone()
         }
 
@@ -74,11 +87,36 @@ mod tests {
     }
 
     #[test]
-    fn rotate_writes_contents_of_input_with_rot13_applied() {
+    fn writes_contents_of_input_with_rot13_applied() {
         let mock_fileio = MockFileIO::new(String::from("Testing, 1, 2, 3"));
         rotate_impl(String::from("in.txt"), String::from("out.txt"), &mock_fileio);
 
         assert_eq!(String::from("out.txt"), *mock_fileio.written_filename.borrow());
         assert_eq!(String::from("Grfgvat, 1, 2, 3"), *mock_fileio.written_contents.borrow());
+    }
+}
+
+#[cfg(test)]
+mod fileio_tests {
+    use super::*;
+
+    #[test]
+    fn read_returns_contents_of_file() {
+        let fileio = StandardFileIO::new();
+        let contents = fileio.read(String::from("in.txt"));
+        assert_eq!(String::from("The dog barks at midnight."), contents);
+    }
+
+    #[test]
+    fn write_writes_contents_to_file() {
+        let filename = String::from("out.txt");
+        std::fs::remove_file(&filename).ok();
+
+        let fileio = StandardFileIO::new();
+        let contents = String::from("The dog barks at midnight.");
+        fileio.write(filename.clone(), contents.clone());
+        assert_eq!(contents, fileio.read(filename.clone()));
+
+        std::fs::remove_file(&filename).ok();
     }
 }
