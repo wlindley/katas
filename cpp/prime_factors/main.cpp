879e474787894b08ba044dab821a9b0e3d5dc4aa
Walker Lindley <walker@walkerlindley.com>
2018-03-08 06:42:36 -0800

    Make driver for prime factors kata

--- cpp/prime_factors/main.cpp
+++ cpp/prime_factors/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
+#include "PrimeFactor.h"
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello, world!" << std::endl;
+	auto value = stoi(std::string(argv[1]));
+	auto factors = primeFactors(value);
+	std::cout << "Prime factors of " << value << ":";
+	for (auto it = factors.begin(); it != factors.end(); it++)
+		std::cout << " " << *it;
+	std::cout << std::endl;
 	return 0;
 }
