e85013bc9b90d62fecc96375d07a7887c35833da
Walker Lindley <walker@walkerlindley.com>
2018-06-19 13:37:16 -0700

    Add concurrent implementation of Add

--- go/stringcalc/calc.go
+++ go/stringcalc/calc.go
@@ -75,3 +75,105 @@ func sum(numbers []int) int {
 	}
 	return total
 }
+
+//ConcurrentAdd concurrently parses a string into numbers and returns the sum
+func ConcurrentAdd(input string) (int, error) {
+	if len(input) == 0 {
+		return 0, nil
+	}
+	numerals := splitIntoNumerals(input)
+	valid, invalid, errs := concurrentNumeralsToInts(numerals)
+
+	total := 0
+	errorFound := false
+	errorMsg := "negatives not allowed"
+	var error error
+	for {
+		select {
+		case n, ok := <-valid:
+			if !ok {
+				valid = nil
+			} else {
+				total += n
+			}
+		case n, ok := <-invalid:
+			if !ok {
+				invalid = nil
+			} else {
+				errorFound = true
+				errorMsg += fmt.Sprintf(" %d", n)
+			}
+		case err, ok := <-errs:
+			if !ok {
+				errs = nil
+			} else if err != nil && error == nil {
+				error = err
+			}
+		}
+
+		if valid == nil && invalid == nil && errs == nil {
+			break
+		}
+	}
+
+	if error != nil {
+		return 0, error
+	}
+	if errorFound {
+		return 0, errors.New(errorMsg)
+	}
+	return total, nil
+}
+
+func concurrentNumeralsToInts(numerals []string) (<-chan int, <-chan int, <-chan error) {
+	numeralChan := numeralsToChannel(numerals)
+	numberChan, errs := numeralsToNumbers(numeralChan)
+	valid, invalid := validateAllNumbers(numberChan)
+	return valid, invalid, errs
+}
+
+func numeralsToChannel(numerals []string) <-chan string {
+	out := make(chan string)
+	go func() {
+		for _, n := range numerals {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func numeralsToNumbers(numerals <-chan string) (<-chan int, <-chan error) {
+	out := make(chan int)
+	errs := make(chan error)
+	go func() {
+		for n := range numerals {
+			val, err := strconv.Atoi(n)
+			if err != nil {
+				errs <- err
+			} else {
+				out <- val
+			}
+		}
+		close(out)
+		close(errs)
+	}()
+	return out, errs
+}
+
+func validateAllNumbers(numbers <-chan int) (<-chan int, <-chan int) {
+	valid := make(chan int)
+	invalid := make(chan int)
+	go func() {
+		for n := range numbers {
+			if n < 0 {
+				invalid <- n
+			} else {
+				valid <- n
+			}
+		}
+		close(valid)
+		close(invalid)
+	}()
+	return valid, invalid
+}
--- go/stringcalc/calc_test.go
+++ go/stringcalc/calc_test.go
@@ -47,3 +47,44 @@ func TestAdd(t *testing.T) {
 		})
 	}
 }
+
+func TestConcurrentAdd(t *testing.T) {
+	tt := []struct {
+		Name     string
+		Input    string
+		Expected int
+		Error    string
+	}{
+		{Name: "Empty String", Input: "", Expected: 0},
+		{Name: "One", Input: "1", Expected: 1},
+		{Name: "Single Invalid", Input: "a", Error: ".*"},
+		{Name: "Two Numbers", Input: "1,2", Expected: 3},
+		{Name: "Many", Input: "1,1,3,5,2,6", Expected: 18},
+		{Name: "Newline", Input: "1\n2,3", Expected: 6},
+		{Name: "Custom Separator", Input: "//;\n1;1;1", Expected: 3},
+		{Name: "Reject Negatives", Input: "2,3,-1", Error: "negatives not allowed -1"},
+		{Name: "Reject Multiple Negatives", Input: "-1,3,-2", Error: "negatives not allowed -1 -2"},
+	}
+
+	for _, x := range tt {
+		t.Run(x.Name, func(st *testing.T) {
+			actual, err := stringcalc.ConcurrentAdd(x.Input)
+
+			if x.Error != "" {
+				if err == nil {
+					st.Error(`Expected an error, but did not receive one`)
+				} else if matches, matchErr := regexp.MatchString(x.Error, err.Error()); matchErr != nil || !matches {
+					st.Errorf(`Expected an error matching "%s", but received "%s"`, x.Error, err.Error())
+				}
+			}
+
+			if x.Error == "" && err != nil {
+				st.Errorf(`Encountered unexpected error: %s`, err.Error())
+			}
+
+			if actual != x.Expected {
+				st.Errorf(`Expected "%s" to be %d, but was %d instead`, x.Input, x.Expected, actual)
+			}
+		})
+	}
+}
